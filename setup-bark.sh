c4353f11f833bd7b97f2d121389c3ee9c53ce708
Luca <longpham.2k.working@gmail.com>
2023-11-07 11:08:22 +0700

    Add script to setup bark lf

---
+++ setup-bark.sh
@@ -0,0 +1,9 @@
+rm -rf bark
+git clone https://huggingface.co/suno/bark
+cd bark
+echo "Pull text_2.pt"
+git lfs pull --include "text_2.pt"
+echo "Pull coarse_2.pt"
+git lfs pull --include "coarse_2.pt"
+echo "Pull fine_2.pt"
+git lfs pull --include "fine_2.pt"
