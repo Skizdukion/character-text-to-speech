466c129f4da752388537a32145d3fdc533cb68ec
Luca <longpham.2k.working@gmail.com>
2023-11-10 12:28:22 +0700

    Update new voice

--- src/schemas.py
+++ src/schemas.py
@@ -5,6 +5,6 @@ from typing import Union, Literal
 class generate_web(BaseModel):
     text: Union[str, None] = None
 
-    char: Literal['obama', 'kera', 'emma'] = Field(...)
+    char: Literal['obama', 'kera', 'emma', 'lena', 'sarah', 'thuytien'] = Field(...)
     class Config:
         orm_mode = True
