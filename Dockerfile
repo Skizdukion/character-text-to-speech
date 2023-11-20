22b73fb934b072f425ff1acc3c311dfa3e51682b
Luca <longpham.2k.working@gmail.com>
2023-11-20 14:36:38 +0700

    Revert back to ogg

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM pytorch/pytorch:2.1.0-cuda11.8-cudnn8-runtime
+FROM redis:6.2.5
+
+CMD [ "redis-server", "--timeout" ]
+
+FROM runpod/pytorch:2.1.0-py3.10-cuda11.8.0-devel-ubuntu22.04
 
 ENV TZ=America/Los_Angeles
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
@@ -14,6 +18,7 @@ RUN wget https://github.com/Skizdukion/character-text-to-speech/raw/main/voices.
 RUN unzip voices.zip && rm voices.zip
 
 ADD ./requirements.txt .
+RUN pip install --ignore-installed blinker
 RUN pip install -r requirements.txt
 ADD ./src .
 
--- src/generate_voice.py
+++ src/generate_voice.py
@@ -15,25 +15,25 @@ def generate_voices(text, char, tts):
         fname = get_random_string(6)
 
         file_name_wav = fname + ".wav"
-        file_name_mp3 = fname + ".mp3"
+        file_name_ogg = fname + ".ogg"
 
         tts.tts_to_file(text=text, voice_dir=os.getcwd()+'/voices', speaker=char, file_path = os.getcwd() + "/" + file_name_wav)
 
         sound = AudioSegment.from_wav(file_name_wav)
 
-        sound.export(file_name_mp3, format="mp3", bitrate="64k")
+        sound.export(file_name_ogg, format="ogg", bitrate="64k", codec="libopus")
 
-        with open(file_name_mp3, "rb") as f:
+        with open(file_name_ogg, "rb") as f:
             audio_content = f.read()
         base64_audio = base64.b64encode(audio_content).decode("utf-8")
 
         res = {"file_base64": base64_audio,
                "audio_text": text,
-               "file_name": file_name_mp3,
+               "file_name": file_name_ogg,
                }
         
         os.remove(file_name_wav)
-        os.remove(file_name_mp3)
+        os.remove(file_name_ogg)
         return res
     except Exception as err:
         res = {"code": 9, "msg": "api error", "err": str(err), "traceback": traceback.format_exc()}
