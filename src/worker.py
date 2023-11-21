0da217a2cf8958c6c2be9cc677e79c26de5c9b59
Luca <longpham.2k.working@gmail.com>
2023-11-21 15:03:04 +0700

    Update syntax

--- src/worker.py
+++ src/worker.py
@@ -13,9 +13,7 @@ from TTS.api import TTS
 from redis import Redis
 # fastapi port
 
-cel_app = Celery('tasks')
-cel_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-cel_app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+cel_app = Celery('tasks', broker="redis://localhost:6379", backend="redis://localhost:6379")
 tts = None
 
 @worker_process_init.connect()
